ea361621677b0a9e1b8673c98fbe1d5d5bee3c53
lakshaydiwan <laxhay666@gmail.com>
2025-04-13 02:17:44 +0530

    fixed editor

--- components/CodeEditor.tsx
+++ components/CodeEditor.tsx
@@ -1,19 +1,29 @@
 'use client'
-import Editor from "@monaco-editor/react";
+import { useEffect, useRef } from "react";
+import Editor, {  OnMount } from "@monaco-editor/react";
+import * as monaco from 'monaco-editor';
 
 function CodeEditor({ value, onChange }: { value: string, onChange: (value: string) => void }) {
-  
+  const editorRef = useRef<monaco.editor.IStandaloneCodeEditor | null>(null);
+
+  const handleEditorDidMount: OnMount = (editor) => {
+    editorRef.current = editor;
+  };
+
+  useEffect(() => {
+    if (editorRef.current) {
+      editorRef.current.setValue(value);
+      editorRef.current.getAction('editor.action.formatDocument')?.run();
+    }
+  }, [value]);
+
   return (
     <Editor
       options={{
+        formatOnType: true,
         formatOnPaste: true,
-        minimap: {
-          enabled: true,
-        },
-        scrollbar: {
-          vertical: "hidden",
-          horizontal: "hidden",
-        },
+        minimap: { enabled: true },
+        scrollbar: { vertical: "hidden", horizontal: "hidden" },
         wordWrap: "on",
         fontSize: 14,
         lineNumbers: "off",
@@ -23,11 +33,12 @@ function CodeEditor({ value, onChange }: { value: string, onChange: (value: stri
         readOnly: true,
       }}
       className="h-full"
+      height={"200px"}
       theme="vs-dark"
-      height="200px"
-      defaultLanguage="JavaScript"
-      defaultValue={value}
+      defaultLanguage="Javascript"
+      value={value}
       onChange={onChange ? (value) => onChange(value as string) : undefined}
+      onMount={handleEditorDidMount}
     />
   );
 }
